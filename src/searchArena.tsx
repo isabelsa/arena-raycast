4faec15d2148a1bb72c5bddafa8c1952fbbfab9a
Isabel Sá <25415758+isabelsa@users.noreply.github.com>
2023-04-26 21:44:36 +0100

    mini util

--- src/searchArena.tsx
+++ src/searchArena.tsx
@@ -1,6 +1,6 @@
-import {useState, useEffect} from "react"
-import {List, Grid, Icon, Image, getPreferenceValues} from "@raycast/api"
-import {getIcon} from "./util"
+import { useState, useEffect } from "react";
+import { List, Grid, Icon, Image, getPreferenceValues } from "@raycast/api";
+import { getIcon, getAccessories } from "./util";
 
 const preferences = getPreferenceValues();
 const Arena = require("are.na");
@@ -12,6 +12,7 @@ interface Channel {
   user: string;
   length: number;
   updated: string;
+  follower_count: string;
 }
 
 interface State {
@@ -19,49 +20,51 @@ interface State {
   items: Channel[];
 }
 
-function search(state: State, setState: any){
-  console.log("trigger")
+function search(state: State, setState: any) {
   arena
-  .search(state.searchText)
-  .channels()
-  .then((channels: any) => {
-    
-    console.log(channels)
+    .search(state.searchText)
+    .channels()
+    .then((channels: any) => {
+      channels.map((chan: any) => {
+        const channel = {
+          index: chan.id,
+          title: chan.title,
+          user: chan.user.full_name,
+          length: chan.length,
+          updated: chan.updated_at,
+        };
 
-    channels.map((chan: any) => {      
-      const channel = {
-        index: chan.id,
-        title: chan.title,
-        user: chan.user.full_name,
-        length: chan.length,
-        updated: chan.updated_at
-      }
-
-     setState((prev) => ({...prev, items: [...channels] }))
+        setState((prev) => ({ ...prev, items: [...channels] }));
+      });
     });
-  });
 }
 
 export default function SearchArena() {
   const [state, setState] = useState<State>({ searchText: "", items: [] });
 
   useEffect(() => {
-    search(state, setState)
+    search(state, setState);
   }, [state.searchText]);
 
-  console.log(state)
-
-
+  console.log(state);
 
   return (
-    <List 
-    navigationTitle="Search for channels"
-    searchBarPlaceholder="Search for channels " 
-    onSearchTextChange={(newValue) => setState((previous) => ({ ...previous, searchText: newValue }))}>
+    <List
+      navigationTitle="Search for channels"
+      searchBarPlaceholder="Search for channels "
+      onSearchTextChange={(newValue) => setState((previous) => ({ ...previous, searchText: newValue }))}
+    >
       {state.searchText === "" && state.items.length === 0 ? (
-        <List.EmptyView icon={{ source: "https://placekitten.com/500/500" }} title="Type something to get started" />
+        <List.EmptyView title="Type something to get started" />
       ) : (
-        state.items.map((item, index) => <List.Item icon={getIcon(1)} key={index} title={item.title} />)
+        state.items.map((item, index) => (
+          <List.Item
+            icon={getIcon(1)}
+            key={index}
+            title={item.title}
+            accessories={getAccessories(item.length.toString(), item.follower_count.toString())}
+          />
+        ))
       )}
     </List>
   );
--- src/util.tsx
+++ src/util.tsx
@@ -1,9 +1,9 @@
-import {Image} from "@raycast/api"
+import { Icon, Image, List } from "@raycast/api";
 
 export function getIcon(text: any): Image.ImageLike {
   const svg = `
 <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40">
-  <rect x="0" y="0" width="40" height="40" fill="#DD7949" rx="10"></rect>
+  <rect x="0" y="0" width="40" height="40" fill="#fff" rx="10"></rect>
   <text
   font-size="22"
   fill="white"
@@ -21,3 +21,11 @@ export function getIcon(text: any): Image.ImageLike {
 }
 
 //Select icon type
+
+export function getAccessories(item: any, itemTwo: any) {
+  const accessories = new Array<List.Item.Accessory>();
+
+  accessories.push({ icon: Icon.AppWindowList, text: `${item} items` });
+
+  return accessories;
+}
